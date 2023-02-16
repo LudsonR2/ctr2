524d3fb12cd0116b1a6587a554b6ef6e4c485d13
Ludson Almeida <ludson.bsa@gmail.com>
2023-02-16 11:44:25 -0300

    public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // set the public path to this directory
-        $this->app->bind('path.public', function() {
-            return base_path().'/public_html';
-        });
+        
     }
 
     /**
